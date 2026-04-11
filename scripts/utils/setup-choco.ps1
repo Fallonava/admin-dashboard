f6f6d9bfac74349315186bbf39de9f145806f038
FaishalFx <238270619+Fallonava@users.noreply.github.com>
2026-04-12 06:09:36 +0700

    chore: clean up temporary python ssh and migration scripts

--- scripts/utils/setup-choco.ps1
+++
@@ -1,3 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://community.chocolatey.org/install.ps1" -OutFile "install-choco.ps1"
-.\install-choco.ps1
